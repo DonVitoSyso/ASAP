902e5cc1cdf9ad2b0dbd18e073d78bd4c5bc2d33
DonVito <vitoss32@mail.ru>
2022-02-15 19:50:12 +0300

    2.Отлучился по делам 19-50

--- functions.py
+++ functions.py
@@ -16,3 +16,17 @@ def counting_even_numbers(col, worksheet):
     print(f'Ответ на вопрос: {worksheet.cell_value(1, 1)}', count)
 
 
+# Счетчик простых чисел в столбце
+def counting_even_numbers(col, worksheet):
+    # Инициализируем счетчик
+    count = 0
+
+    for cell in worksheet.col(col):
+        try:
+            if not (float(cell.value) % 2):
+                count += 1
+        except ValueError:
+            pass
+
+    print(f'Ответ на вопрос: {worksheet.cell_value(1, 1)}', count)
+
---
+++ utils.py
@@ -0,0 +1,2 @@
+def even_numbers(num):
+    if num%2
