aeb4f9e1db17e21ded3ceaf42997b5a285fa32d5
DonVito <vitoss32@mail.ru>
2022-02-16 18:33:29 +0300

    2.Сколько простых чисел закончил в 17-50

--- Table.py
+++ Table.py
@@ -9,5 +9,4 @@ worksheet = workbook.sheet_by_index(1)
 # Пройдемся по столбцу 2 и посчитаем все четные числа
 counting_even_numbers(1, worksheet)
 # Пройдемся по столбцу 3 и посчитаем простые числа
-counting_even_numbers(2, worksheet)
-
+counting_prime_numbers(2, worksheet)
--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import xlrd
+from utils import *
 
 
 # Счетчик четных чисел в столбце
@@ -8,25 +9,24 @@ def counting_even_numbers(col, worksheet):
 
     for cell in worksheet.col(col):
         try:
-            if not (float(cell.value) % 2):
+            if not (int(cell.value) % 2):
                 count += 1
         except ValueError:
             pass
 
-    print(f'Ответ на вопрос: {worksheet.cell_value(1, 1)}', count)
+    print(f'Ответ на вопрос: {worksheet.cell_value(1, col)}', count)
 
 
 # Счетчик простых чисел в столбце
-def counting_even_numbers(col, worksheet):
+def counting_prime_numbers(col, worksheet):
     # Инициализируем счетчик
     count = 0
 
     for cell in worksheet.col(col):
         try:
-            if not (float(cell.value) % 2):
+            if not (even_numbers(int(cell.value))):
                 count += 1
         except ValueError:
             pass
 
-    print(f'Ответ на вопрос: {worksheet.cell_value(1, 1)}', count)
-
+    print(f'Ответ на вопрос: {worksheet.cell_value(1, col)}', count)
